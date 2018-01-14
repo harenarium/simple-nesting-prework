ff38e75e8563e231757ee4078122eb4cb3b8482d
Caroline Lee <carolinel2610@gmail.com>
2018-01-14 18:57:22 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-	programmer_hash[:dennis_ritchie][:languages]
+	programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,6 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
+	programmer_hash[:yukihiro_matsumoto]= {:known_for => "Ruby", :languages => ["LISP, C"]}
 
 
 end
@@ -103,6 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
+		 programmer_hash[:alan_kay][:known_for]= alans_new_info
 
 
 end
@@ -123,5 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
 end
